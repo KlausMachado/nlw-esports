ef58852f0f96a884ff24cac264738ba25bda92f2
Carla Machado <carlamrosa438@gmail.com>
2022-09-13 23:46:17 -0300

    updates styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,19 @@
-//definindo a tipagem das propriedades que o componente Button devem seguir
-interface ButtonProps {
-  title: string;
-}
+import './styles/main.css'
 
-function Button(props: ButtonProps /* definimos que as props devem vir de ButtonProps*/) {
-  return <button>{props.title}</button>;
-}
+// //definindo a tipagem das propriedades que o componente Button devem seguir
+// interface ButtonProps {
+//   title: string;
+// }
+
+// function Button(props: ButtonProps /* definimos que as props devem vir de ButtonProps*/) {
+//   return <button>{props.title}</button>;
+// }
 
 function App() {
   return (
-    <>
-      <h1>Hello Word</h1>
-      <Button title="send 1" />
-      <Button title="send 2" />
-      <Button title="send 3" />
-    </>
+    <div className='w-8 h-8 bg-violet-500'>
+
+    </div>
   );
 }
 
